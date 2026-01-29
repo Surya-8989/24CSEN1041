79172eb9bd3773101cc433ecda07d4ba47f9860c
Surya-8989 <hdabbiru@student.gitam.edu>
2026-01-29 15:34:40 +0530

    Add user login functionality with credential check

---
+++ user login.py
@@ -0,0 +1,14 @@
+x = "python"
+y = "python1234"
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == x and password == y:
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
+sample output:
+username=hi
+password=hhii
+invalid credentials
